7107b9a2c017ce7bbccba2e29e5118ab499eaac0
ordazhoyos2001@gmail.com <ordazhoyos2001@gmail.com>
2025-10-04 23:21:48 -0500

    xx

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -29,6 +29,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
+      <head>
+        {/* 👇 Script de Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-JDFP37FYVR"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-JDFP37FYVR');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
