e0b808597a3783c65d419da5c1a9f72a680a13c9
ordazhoyos2001@gmail.com <ordazhoyos2001@gmail.com>
2025-10-04 23:36:33 -0500

    nuevo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { Providers } from './providers';
+import Script from 'next/script';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
